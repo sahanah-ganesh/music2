203be67a920eefde51a8dc9d5ea97aa1f7f991c1
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-10 15:13:04 +0000

    completed

--- music2.js
+++ music2.js
@@ -1,3 +1,76 @@
+//---------------------------------------------
+function Library(name, creator) {
+  this.name = name;
+  this.creator = creator;
+  this.playlists = [];
+}
+
+function Playlist(name) {
+  this.name = name;
+  this.tracks = [];
+}
+
+function Track(title, rating, length) {
+  this.title = title;
+  this.rating = rating;
+  this.length = length;
+}
+//--------------------------------------------------------------
+
+Library.prototype.addPlaylist = function (Playlist) {
+  this.playlists.push(Playlist);
+}
+
+Playlist.prototype.addTrack = function (Track) {
+  this.tracks.push(Track);
+}
+
+Playlist.prototype.overallRating = function overallRating () {
+  const ratings = this.tracks.map(track => track.rating);
+  const sum = ratings.reduce((acc, c) => acc + c, 0);
+  return Math.round(sum / ratings.length);
+}
+Playlist.prototype.totalDuration = function totalDuration () {
+  const lengths = this.tracks.map(track => track.length);
+  const sum = lengths.reduce((acc, c) => acc + c, 0);
+  return Math.round(sum / lengths.length);
+}
+
+//---------------------------------------------------------------
+
+let newLibrary = new Library('Oldies', 'John');
+
+let playlist1 = new Playlist('Marley');
+
+let track1 = new Track('Buffalo Soldier', 4, 180);
+let track2 = new Track('I shot the sheriff', 2, 140);
+let track3 = new Track('No woman no cry', 5, 160);
+
+let playlist2 = new Playlist('Queen');
+
+let track4 = new Track('Killer Queen', 1, 300);
+let track5 = new Track('Under Pressure', 5, 200);
+let track6 = new Track('We Will Rock You', 3, 400);
+
+playlist1.addTrack(track1);
+playlist1.addTrack(track2);
+playlist1.addTrack(track3);
+
+playlist2.addTrack(track4);
+playlist2.addTrack(track5);
+playlist2.addTrack(track6);
+
+newLibrary.addPlaylist(playlist1);
+newLibrary.addPlaylist(playlist2);
+
+//--------------------------------------------------------------
+
+// console.log(newLibrary);
+// console.log(newLibrary.playlists);
+// console.log(newLibrary.playlists[0]);
+// console.log(newLibrary.playlists[0].totalDuration());
+// console.log(newLibrary.playlists[1].overallRating());
+
 // let musicLibrary = {
 //   name: name,
 //   creator: creator,
@@ -38,55 +111,6 @@
 //     }
 //   ]
 // }
-//---------------------------------------------
-function Library(name, creator) {
-  this.name = name;
-  this.creator = creator;
-  this.playlists = [];
-}
-
-function Playlist(name) {
-  this.name = name;
-  this.tracks = [];
-  this.overallRating = this.tracks.map((track) => {
-    reduce((a, b) => a + b) / rating;
-}
-
-function Track(title, rating, length) {
-  this.title = title;
-  this.rating = rating;
-  this.length = length;
-}
-//-----------------------------------------------
-
-let newLibrary = new Library('Oldies', 'John');
-
-let playlist1 = new Playlist('Marley');
-
-let track1 = new Track('Buffalo Soldier', '4', '180');
-let track2 = new Track('I shot the sheriff', '2', '140');
-let track3 = new Track('No woman no cry', '5', '160');
-
-let playlist2 = new Playlist('Queen');
-
-let track4 = new Track('Killer Queen', '1', '300');
-let track5 = new Track('Under Pressure', '5', '200');
-let track6 = new Track('We Will Rock You', '3', '400');
-
-playlist1.tracks.push(track1, track2, track3);
-playlist2.tracks.push(track4, track5, track6);
-
-newLibrary.playlists.push(playlist1, playlist2);
-
-// console.log(newLibrary);
-// console.log(playlist2.tracks);
-
-function rating() {
-  if (newPlaylist.playlists.tracks) {
-
-  }
-}
-
 
 // prototype functions:
 // Library
